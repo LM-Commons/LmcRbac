60b8ca715a73233bf9b92dbf69179483b1af144e
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2024-07-08 17:57:59 -0400

    Adding Identity providers services (#49)
    Signed-off-by: Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>

---
+++ test/Identity/AuthenticationIdentityProviderTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace LmcRbacTest\Identity;
+
+use LmcRbac\Identity\AuthenticationIdentityProvider;
+
+/**
+ * @covers \LmcRbac\Identity\AuthenticationIdentityProvider
+ */
+class AuthenticationIdentityProviderTest extends \PHPUnit\Framework\TestCase
+{
+    protected AuthenticationIdentityProvider $identityProvider;
+
+    /**
+     * @var \Laminas\Authentication\AuthenticationService|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $authenticationService;
+
+    public function setUp(): void
+    {
+        $this->authenticationService = $this->createMock('Laminas\Authentication\AuthenticationService');
+        $this->identityProvider = new AuthenticationIdentityProvider($this->authenticationService);
+    }
+
+    public function testCanReturnIdentity()
+    {
+        $identity = $this->createMock('LmcRbac\Identity\IdentityInterface');
+
+        $this->authenticationService->expects($this->once())
+                                    ->method('getIdentity')
+                                    ->willReturn($identity);
+
+        $this->assertSame($identity, $this->identityProvider->getIdentity());
+    }
+}
